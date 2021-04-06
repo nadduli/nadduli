e5f80f1b0081d53991efc6240d89245641828892
Nadduli Daniel <80126109+nadduli@users.noreply.github.com>
2021-04-07 01:38:58 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,2 +1,10 @@
-let header = document.getElementById("nav");
+let navigation = document.getElementById("nav-sec");
 let links = document.getElementsByClassName("link");
+//using for loop to loop through an array of links
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function(){
+        let current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+    })
+}
